e46376a38c7f36f2c258b4f1e5029678a92debdd
Adrian Dorey <144232716+adorey91@users.noreply.github.com>
2023-12-04 13:25:13 -0400

    Added character at the end

--- Assets/Scripts/endTrigger.cs
+++ Assets/Scripts/endTrigger.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class endTrigger : MonoBehaviour
 {
     public GameObject endRun;
+    public GameObject character;
     [SerializeField] private Light spotLight;
 
     public void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Player")
         {
+            character.SetActive(true);
             endRun.SetActive(true);
             spotLight.enabled = false;
         }
